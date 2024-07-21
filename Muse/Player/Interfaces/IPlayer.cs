59f61da3ab49d55e2e4141a0efaab3fcb0f258a2
Maciej Winnik <maciekwin3@gmail.com>
2024-07-21 21:28:13 +0200

    Remove specific platform players implementation

--- Muse/Player/Interfaces/IPlayer.cs
+++ Muse/Player/Interfaces/IPlayer.cs
@@ -2,13 +2,10 @@ namespace Muse.Player.Interfaces;
 
 public interface IPlayer
 {
-   event EventHandler PlaybackFinished;
-   bool Playing { get; }
-   bool Paused { get; }
-
-   Task Play(string fileName);
-   Task Pause();
-   Task Resume();
-   Task Stop();
-   Task SetVolume(byte percent);
+    void Load(string fileName);
+    void Play();
+    void Pause();
+    void Resume();
+    void Stop();
+    void SetVolume(int percent);
 }
