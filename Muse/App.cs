d1714b5309bbc1f67b817d6b42d1d261a97f8dd6
Maciej Winnik <maciekwin3@gmail.com>
2024-07-21 21:28:42 +0200

    Use new player and connect it to basic UI

--- Muse/App.cs
+++ Muse/App.cs
@@ -1,4 +1,3 @@
-using System.Net.Mime;
 using Muse.Player.Interfaces;
 using Muse.Windows;
 using Terminal.Gui;
@@ -7,55 +6,56 @@ namespace Muse;
 
 public class App
 {
-   private readonly IPlayer player;
+    private readonly IPlayer player;
 
-   public App(IPlayer player)
-   {
-      this.player = player;
-   }
-   
-   public void Run(string[] args)
-   {
-      Application.Init();
-      var win = new MainWindow(player);
-      Colors.Base.Normal = Application.Driver.MakeAttribute(Color.Green, Color.Black);
-      Colors.Menu.Normal = Application.Driver.MakeAttribute(Color.Blue, Color.BrightYellow);
-      Application.Top.Add(CreateMenuBar());
-      //Application.Top.Add(InitLabel(win));
-      Application.Top.Add(win);
-      Application.Run();
-   }
+    public App(IPlayer player)
+    {
+        this.player = player;
+    }
 
-   private MenuBar CreateMenuBar()
-   {
-      return new MenuBar(new MenuBarItem[]
-      {
-         new MenuBarItem("File", new MenuItem[]
-         {
-            new MenuItem("Quit", "", () => Quit())
-         }),
-         new MenuBarItem("Help", new MenuItem[]
-         {
-            new MenuItem("About", "", ()
-               => MessageBox.Query(49, 5, "About", "Written by Maciej Winnik", "Ok"))
-         })
-      });
-   }
+    public void Run(string[] args)
+    {
+        Application.Init();
+        var win = new MainWindow(player);
+        Application.Run(win);
+        //Colors.Base.Normal = Application.Driver.MakeAttribute(Color.Green, Color.Black);
+        //Colors.Menu.Normal = Application.Driver.MakeAttribute(Color.Blue, Color.BrightYellow);
+        //Application.Top.Add(CreateMenuBar());
+        //Application.Top.Add(InitLabel(win));
+        Application.Shutdown();
+    }
 
-   private Label InitLabel(View parent)
-   {
-      var label2 = new Label("Hello, World from Sopot!")
-      {
-         X = Pos.Center(),
-         Y = Pos.Bottom(parent),
-         Height = 1
-      };
+    //private MenuBar CreateMenuBar()
+    //{
+    //    return new MenuBar(new MenuBarItem[]
+    //    {
+    //     new MenuBarItem("File", new MenuItem[]
+    //     {
+    //        new MenuItem("Quit", "", () => Quit())
+    //     }),
+    //     new MenuBarItem("Help", new MenuItem[]
+    //     {
+    //        new MenuItem("About", "", ()
+    //           => MessageBox.Query(49, 5, "About", "Written by Maciej Winnik", "Ok"))
+    //     })
+    //    });
+    //}
 
-      return label2;
-   }
+    private Label InitLabel(View parent)
+    {
+        var label2 = new Label()
+        {
+            Text = "Hello, World from Sopot!",
+            X = Pos.Center(),
+            Y = Pos.Bottom(parent),
+            Height = 1
+        };
 
-   void Quit()
-   {
-      Application.RequestStop();
-   }
+        return label2;
+    }
+
+    void Quit()
+    {
+        Application.RequestStop();
+    }
 }
--- Muse/Windows/MainWindow.cs
+++ Muse/Windows/MainWindow.cs
@@ -6,8 +6,10 @@ namespace Muse.Windows;
 public class MainWindow : Window
 {
     private readonly IPlayer player;
-    
-    public MainWindow(IPlayer player) : base("Muse")
+    private Label label = null!;
+    private Slider volumeSlider = null!;
+
+    public MainWindow(IPlayer player)
     {
         this.player = player;
         InitControls();
@@ -16,44 +18,72 @@ public class MainWindow : Window
 
     public void InitControls()
     {
-        Add(InitLabel());
+        Add(InitLabel("Hello, World!"));
         Add(InitPlayPauseButton());
+        Add(InitVolumeSlider());
     }
 
     public void InitStyles()
     {
-       X = 0;
-       Y = 1;
-       Width = Dim.Fill();
-       Height = Dim.Fill();
+        X = 0;
+        Y = 1;
+        Width = Dim.Fill();
+        Height = Dim.Fill();
     }
 
-    private Label InitLabel()
+    private Label InitLabel(string text)
     {
-       return new Label("Hello, World!")
-          {
-             X = Pos.Center(),
-             Y = Pos.Center(),
-             Height = 1,
-          };
+        label = new Label()
+        {
+            Text = text,
+            X = Pos.Center(),
+            Y = Pos.Center(),
+            Height = 1,
+        };
+        return label;
     }
-   
+
+    private Slider InitVolumeSlider()
+    {
+        var options = new List<object> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        volumeSlider = new Slider(options)
+        {
+            X = Pos.Center(),
+            Y = 4,
+            Width = Dim.Fill(),
+            Type = SliderType.Single,
+        };
+
+        volumeSlider.OptionsChanged += (sender, e) =>
+        {
+            var value = e.Options.FirstOrDefault().Key;
+            player.SetVolume(value);
+        };
+
+        return volumeSlider;
+    }
+
     private Button InitPlayPauseButton()
     {
-        var button = new Button("||")
+        var button = new Button()
         {
+            Text = "||",
             X = Pos.Center(),
             Y = Pos.Bottom(this) - 4,
             Height = 1,
         };
-        button.Clicked += () =>
+
+        button.Accept += (sender, e) =>
         {
             if (button.Text == "|>")
             {
                 button.Text = "||";
-                player.Play(@"C:\Users\Maciek\Music\Andzia.mp3");
+                var song = @"C:\Users\macie\Music\Miszmasz\Dua Lipa - Houdini (Official Music Video).mp3";
+                label.Text = "Playing: " + song;
+                player.Load(song);
+                player.Play();
             }
-            else 
+            else
             {
                 button.Text = "|>";
                 player.Pause();
