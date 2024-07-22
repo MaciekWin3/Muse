d8473a01630f3a2444410c159d11b21bd9c75fee
Maciej Winnik <maciekwin3@gmail.com>
2024-07-22 17:22:25 +0200

    Ascii art

--- Muse/App.cs
+++ Muse/App.cs
@@ -1,11 +1,14 @@
 using Muse.Player.Interfaces;
 using Muse.Windows;
+using System.Text;
 using Terminal.Gui;
 
 namespace Muse;
 
+public class App : Toplevel
 {
     private readonly IPlayer player;
+    private MenuBar menuBar = null!;
 
     public App(IPlayer player)
     {
@@ -22,7 +25,6 @@ namespace Muse;
         Application.Shutdown();
     }
 
-
     private MenuBar InitMenuBar()
     {
         menuBar = new MenuBar
@@ -31,6 +33,7 @@ namespace Muse;
             [
                 new("_File", new MenuItem[]
                 {
+                    new("_Quit", "", () => Application.RequestStop())
                 }),
                 new("_Help", new MenuItem[]
                 {
@@ -42,6 +45,18 @@ namespace Muse;
         return menuBar;
     }
 
+    private void ShowAsciiArt()
     {
+        var sb = new StringBuilder();
+        sb.AppendLine("Muse - Terminal MP3 player");
+        sb.AppendLine();
+        sb.AppendLine(@"  __  __ _    _  _____ ______ ");
+        sb.AppendLine(@" |  \/  | |  | |/ ____|  ____|");
+        sb.AppendLine(@" | \  / | |  | | (___ | |__   ");
+        sb.AppendLine(@" | |\/| | |  | |\___ \|  __|  ");
+        sb.AppendLine(@" | |  | | |__| |____) | |____ ");
+        sb.AppendLine(@" |_|  |_|\____/|_____/|______|");
+        sb.AppendLine();
+        MessageBox.Query(50, 12, "About", sb.ToString(), "Ok");
     }
 }
