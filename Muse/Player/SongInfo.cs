6662d7945dddf43361e1f5fc9608d7c4bf6cb5d5
Maciej Winnik <maciekwin3@gmail.com>
2024-07-22 22:00:33 +0200

    SongInfo changes

--- Muse/Player/SongInfo.cs
+++ Muse/Player/SongInfo.cs
@@ -5,7 +5,7 @@ namespace Muse.Player
     public struct SongInfo(AudioFileReader audioFileReader)
     {
         public readonly string Name => Path.GetFileName(audioFileReader.FileName);
-        public readonly double TotalTimeInSeconds => audioFileReader.TotalTime.TotalSeconds;
-        public readonly double CurrentTime => (audioFileReader.CurrentTime.Hours * 60 * 60) + (audioFileReader.CurrentTime.Minutes * 60) + audioFileReader.CurrentTime.Seconds;
+        public readonly int TotalTimeInSeconds => (int)audioFileReader.TotalTime.TotalSeconds;
+        public readonly int CurrentTime => (audioFileReader.CurrentTime.Hours * 60 * 60) + (audioFileReader.CurrentTime.Minutes * 60) + audioFileReader.CurrentTime.Seconds;
     }
 }
