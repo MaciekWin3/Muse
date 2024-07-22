4c06c9a1f8ba06a0d1236712765d53fb29c331a3
Maciej Winnik <maciekwin3@gmail.com>
2024-07-22 16:40:15 +0200

    Add Result class

---
+++ Muse/Player/Utils/ResultT.cs
@@ -0,0 +1,27 @@
+﻿namespace Muse.Player.Utils
+{
+    public class Result<T> : Result
+    {
+        protected internal Result(T value, bool success, string error)
+            : base(success, error)
+        {
+            Value = value;
+        }
+        public T Value { get; set; }
+
+        public static implicit operator Result<T>(string error) => Fail<T>(error);
+        public static implicit operator Result<T>(T value) => Ok(value);
+
+        public T GetValue()
+        {
+            if (Success && Value != null)
+            {
+                return Value;
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(Value));
+            }
+        }
+    }
+}
