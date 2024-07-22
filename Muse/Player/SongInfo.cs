b580a5f2c3ad6e465271a3ec45a069329e22c96e
Maciej Winnik <maciekwin3@gmail.com>
2024-07-22 17:24:58 +0200

    Introduce result type to player

---
+++ Muse/Player/SongInfo.cs
@@ -0,0 +1,11 @@
+﻿using NAudio.Wave;
+
+namespace Muse.Player
+{
+    public struct SongInfo(AudioFileReader audioFileReader)
+    {
+        public string Name => audioFileReader.FileName;
+        public double TotalTimeInSeconds => audioFileReader.TotalTime.TotalSeconds;
+        public double CurrentTime => audioFileReader.CurrentTime.Seconds;
+    }
+}
