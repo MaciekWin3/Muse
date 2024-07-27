c9e46bc411938e11a4acde04557d419ec4d2c913
Maciej Winnik <maciekwin3@gmail.com>
2024-07-27 12:13:15 +0200

    Download file from yt

--- src/Muse/App.cs
+++ src/Muse/App.cs
@@ -2,11 +2,14 @@ using Muse.Player;
 using Muse.Windows;
 using System.Text;
 using Terminal.Gui;
+using VideoLibrary;
 
 namespace Muse;
 
 public class App : Toplevel
 {
+    private readonly static string MUSIC_DIRECTORY = @"C:\\Users\\macie\\Music\\Miszmasz\\";
+
     private readonly IPlayer player;
     private MenuBar menuBar = null!;
     private StatusBar statusBar = null!;
@@ -41,6 +44,10 @@ public class App : Toplevel
                 new("_Help", new MenuItem[]
                 {
                     new("_About", "", () => ShowAsciiArt())
+                }),
+                new("_Download", new MenuItem[]
+                {
+                    new("_From YT", "", () => ShowDownloadDialog())
                 })
             ]
         };
@@ -73,5 +80,49 @@ public class App : Toplevel
         sb.AppendLine(@" |_|  |_|\____/|_____/|______|");
         sb.AppendLine();
         MessageBox.Query(50, 12, "About", sb.ToString(), "Ok");
+    }
+
+
+    private void ShowDownloadDialog()
+    {
+        var button = new Button()
+        {
+            Title = "Download",
+        };
+
+        var urlTextField = new TextField()
+        {
+            X = 1,
+            Y = 1,
+            Width = Dim.Fill(),
+        };
+
+        var spinnerView = new SpinnerView { X = Pos.Bottom(urlTextField) + 1, Y = 0, Visible = false };
+
+        button.Accept += (s, e) =>
+        {
+            spinnerView.Visible = true;
+            var url = urlTextField.Text;
+            SaveVideoToDisk(url);
+            Application.Refresh();
+            spinnerView.Visible = false;
+        };
+
+        var dialog = new Dialog()
+        {
+            Title = "Download",
+            Width = Dim.Percent(50),
+            Height = Dim.Percent(50),
+        };
+        dialog.Add(urlTextField);
+        dialog.AddButton(button);
+        Application.Run(dialog);
+    }
+
+    public void SaveVideoToDisk(string link)
+    {
+        var youTube = YouTube.Default;
+        var video = youTube.GetVideo(link);
+        File.WriteAllBytes(MUSIC_DIRECTORY + video.FullName, video.GetBytes());
     }
 }
