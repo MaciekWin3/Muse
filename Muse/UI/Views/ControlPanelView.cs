91cd1da532ec4cf3b0dbc96f154faf7596028e57
Maciej Winnik <maciekwin3@gmail.com>
2026-03-06 19:11:51 +0100

    Move Repeat and Shuffle controls to StatusBar
    - Removed Repeat and Shuffle buttons from ControlPanelView.
    - Added Repeat and Shuffle shortcuts (R/S) to StatusBarView.
    - Updated status bar to dynamically show current play mode and shuffle status.

--- Muse/UI/Views/ControlPanelView.cs
+++ Muse/UI/Views/ControlPanelView.cs
@@ -17,8 +17,6 @@ public class ControlPanelView : FrameView
     private Button backButton = null!;
     private Button nextSongButton = null!;
     private Button previousSongButton = null!;
-    private Button repeatButton = null!;
-    private Button shuffleButton = null!;
 
     public ControlPanelView(IUiEventBus uiBus, Pos x, Pos y)
     {
@@ -48,28 +46,6 @@ public class ControlPanelView : FrameView
             Application.Invoke(() =>
             {
                 playPauseButton.Text = "|>";
-            });
-        });
-
-        uiBus.Subscribe<PlayModeChanged>(msg =>
-        {
-            Application.Invoke(() =>
-            {
-                repeatButton.Text = msg.NewMode switch
-                {
-                    PlayMode.None => "Repeat: None",
-                    PlayMode.Repeat => "Repeat: All",
-                    PlayMode.RepeatOne => "Repeat: One",
-                    _ => "Repeat"
-                };
-            });
-        });
-
-        uiBus.Subscribe<ShuffleChanged>(msg =>
-        {
-            Application.Invoke(() =>
-            {
-                shuffleButton.Text = msg.IsShuffle ? "Shuffle: On" : "Shuffle: Off";
             });
         });
     }
@@ -146,40 +122,10 @@ public class ControlPanelView : FrameView
             e.Handled = true;
         };
 
-        repeatButton = new Button()
-        {
-            Text = "Repeat: None",
-            Height = ButtonsHeight,
-            X = 0,
-            ShadowStyle = ShadowStyle.None,
-        };
-
-        repeatButton.Accepting += (s, e) =>
-        {
-            uiBus.Publish(new TogglePlayModeRequested());
-            e.Handled = true;
-        };
-
-        shuffleButton = new Button()
-        {
-            Text = "Shuffle: Off",
-            Height = ButtonsHeight,
-            X = Pos.AnchorEnd(15), 
-            ShadowStyle = ShadowStyle.None,
-        };
-
-        shuffleButton.Accepting += (s, e) =>
-        {
-            uiBus.Publish(new ShuffleToggleRequested());
-            e.Handled = true;
-        };
-
-        Add(repeatButton);
         Add(previousSongButton);
         Add(backButton);
         Add(playPauseButton);
         Add(forwardButton);
         Add(nextSongButton);
-        Add(shuffleButton);
     }
 }
